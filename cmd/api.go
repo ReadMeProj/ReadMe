456f5125fac9c38d401d11d462edcd430f661c84
Yariv Levy <yarivlevy81@gmail.com>
2021-04-24 01:48:07 +0300

    api.go formatting

--- cmd/api.go
+++ cmd/api.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-    "net/http"
-   
-    "ReadMe/internal/db"
-    "github.com/gorilla/mux"
-    "ReadMe/internal/api"
+	"net/http"
+
+	"ReadMe/internal/api"
+	"ReadMe/internal/db"
+	"github.com/gorilla/mux"
 )
 
 func getUser(responseWriter http.ResponseWriter, r *http.Request) {
-    jsonData, err := dBase.GetUser(
-        db.ID(api.ExtractIDStringFromRequest(r)),
-    )
-    response := api.Response{Error: err, Data: jsonData}
+	jsonData, err := dBase.GetUser(
+		db.ID(api.ExtractIDStringFromRequest(r)),
+	)
+	response := api.Response{Error: err, Data: jsonData}
 
-    api.GenerateHandler(responseWriter, r, response)
+	api.GenerateHandler(responseWriter, r, response)
 }
 
 func getUsers(responseWriter http.ResponseWriter, r *http.Request) {
-    jsonData, err := dBase.GetUsers()
-    response := api.Response{Error: err, Data: jsonData}
+	jsonData, err := dBase.GetUsers()
+	response := api.Response{Error: err, Data: jsonData}
 
-    api.GenerateHandler(responseWriter, r, response)
+	api.GenerateHandler(responseWriter, r, response)
 }
 
 func getArticle(responseWriter http.ResponseWriter, r *http.Request) {
-    jsonData, err := dBase.GetArticle(
-        db.ID(api.ExtractIDStringFromRequest(r)),
-    )
-    response := api.Response{Error: err, Data: jsonData}
-    api.GenerateHandler(responseWriter, r, response)
+	jsonData, err := dBase.GetArticle(
+		db.ID(api.ExtractIDStringFromRequest(r)),
+	)
+	response := api.Response{Error: err, Data: jsonData}
+	api.GenerateHandler(responseWriter, r, response)
 }
 
 func getArticles(responseWriter http.ResponseWriter, r *http.Request) {
-    jsonData, err := dBase.GetArticles()
-    response := api.Response{Error: err, Data: jsonData}
-    api.GenerateHandler(responseWriter, r, response)
+	jsonData, err := dBase.GetArticles()
+	response := api.Response{Error: err, Data: jsonData}
+	api.GenerateHandler(responseWriter, r, response)
 }
 
 var dBase db.ReadMeDatabase
 
 func main() {
-    router := mux.NewRouter()
-    
-    dBase = db.NewMongoController()
+	router := mux.NewRouter()
 
-    // REST API
-    router.HandleFunc("/api/getUser/{id}", getUser).Methods("GET") 
-    router.HandleFunc("/api/getUsers", getUsers).Methods("GET")
-    router.HandleFunc("/api/getArticle/{id}", getArticle).Methods("GET") 
-    router.HandleFunc("/api/getArticles", getArticles).Methods("GET")
+	dBase = db.NewMongoController()
 
-    http.ListenAndServe(":8081", router)
+	// REST API
+	router.HandleFunc("/api/getUser/{id}", getUser).Methods("GET")
+	router.HandleFunc("/api/getUsers", getUsers).Methods("GET")
+	router.HandleFunc("/api/getArticle/{id}", getArticle).Methods("GET")
+	router.HandleFunc("/api/getArticles", getArticles).Methods("GET")
+
+	http.ListenAndServe(":8081", router)
 
 }
