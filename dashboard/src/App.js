986e7ffcfc532bcca032eb54022a3202ddaf5508
RonitPriav <42570726+RonitPr@users.noreply.github.com>
2021-04-28 01:05:16 +0300

    put base for using api in the dashboard, started work on login page

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { Component } from "react";
 import LoginPage from "./pages/LoginPage";
 import RequestsPage from "./pages/RequestsPage";
 import NavBar from "./components/NavBar";
@@ -8,108 +9,126 @@ import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 import ArticleList from "./components/ArticleList";
 import logo from "./assets/siteLogo.png";
 
-const posts = [
-  {
-    id: "1",
-    name: "This first post is about React",
-    content:
-      "malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,",
-    link: "",
-  },
-  {
-    id: "2",
-    name: "This next post is about Preact",
-    content:
-      "tempor Pellentesque habitant morbi tristique senectus et netus et malesuada",
-    link: "",
-  },
-  {
-    id: "3",
-    name: "We have yet another React post!",
-    content:
-      "fames ac turpis egestas. Vestibulum tortor quam, feugiat  vitae, ultricies eget, tempor sit amet, ante.",
-    link: "",
-  },
-  {
-    id: "4",
-    name: "This is the fourth and final post",
-    content:
-      "Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
-    link: "",
-  },
-];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articlesData: [
+        {
+          id: "1",
+          name: "This first post is about React",
+          content:
+            "malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,",
+          link: "",
+        },
+        {
+          id: "2",
+          name: "This next post is about Preact",
+          content:
+            "tempor Pellentesque habitant morbi tristique senectus et netus et malesuada",
+          link: "",
+        },
+        {
+          id: "3",
+          name: "We have yet another React post!",
+          content:
+            "fames ac turpis egestas. Vestibulum tortor quam, feugiat  vitae, ultricies eget, tempor sit amet, ante.",
+          link: "",
+        },
+        {
+          id: "4",
+          name: "This is the fourth and final post",
+          content:
+            "Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
+          link: "",
+        },
+      ],
+    };
+  }
 
-function App() {
-  const { search } = window.location;
-  const query = new URLSearchParams(search).get("q");
-  return (
-    <div className="App">
-      <Router>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col">
-              <div className="row align-items-center">
-                <div className="col-sm">
-                  <div>
-                    <Link to="/login" className="btn btn-primary">
-                      Login
-                    </Link>
+  //   componentDidMount() {
+  //     fetch("http://localhost:8081/api/getArticles")
+  //       .then((response) => response.json())
+  //       .then((data) => this.setState({ articlesData: data.Data }));
+  //   }
+
+  render() {
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get("q");
+    return (
+      <div className="App">
+        <Router>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col">
+                <div className="row align-items-center">
+                  <div className="col-sm">
+                    <div>
+                      <Link to="/login" className="btn btn-primary">
+                        Login
+                      </Link>
+                    </div>
+                  </div>
+                  <div className="col-sm">
+                    <div>
+                      <SearchBar />
+                    </div>
+                  </div>
+                  <div className="col-sm">
+                    <img
+                      width={100}
+                      height={20}
+                      className="float-right"
+                      src={logo}
+                      alt="Generic placeholder"
+                    />
                   </div>
                 </div>
-                <div className="col-sm">
-                  <div>
-                    <SearchBar />
-                  </div>
-                </div>
-                <div className="col-sm">
-                  <img
-                    width={100}
-                    height={20}
-                    className="float-right"
-                    src={logo}
-                    alt="Generic placeholder"
-                  />
-                </div>
+                <div className="row"></div>
               </div>
-              <div className="row"></div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-sm" />
-            <div className="col-sm">
-              <SearchFilterBox />
+            <div className="row">
+              <div className="col-sm" />
+              <div className="col-sm">
+                <SearchFilterBox />
+              </div>
+              <div className="col-sm" />
             </div>
-            <div className="col-sm" />
-          </div>
-          <div className="row">
-            <div className="col">
-              <hr />
+            <div className="row">
+              <div className="col">
+                <hr />
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-2">
-              <NavBar />
-            </div>
-            <div className="col-10">
-              <div className="App">
-                <Switch>
-                  <Route exact path="/">
-                    <ArticleList data={posts} query={query} />
-                  </Route>
-                  <Route path="/login">
-                    <LoginPage />
-                  </Route>
-                  <Route path="/requests">
-                    <RequestsPage />
-                  </Route>
-                </Switch>
+            <div className="row">
+              <div className="col-2">
+                <NavBar />
+              </div>
+              <div className="col-10">
+                <div className="App">
+                  <Switch>
+                    <Route exact path="/">
+                      <div>
+                        <ArticleList
+                          data={this.state.articlesData}
+                          query={query}
+                        />
+                      </div>
+                    </Route>
+                    <Route path="/login">
+                      <LoginPage />
+                    </Route>
+                    <Route path="/requests">
+                      <RequestsPage />
+                    </Route>
+                  </Switch>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Router>
-    </div>
-  );
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- dashboard/src/pages/LoginPage.js
+++ dashboard/src/pages/LoginPage.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class LoginPage extends Component {
   state = {};
   render() {
-    return <div></div>;
+    return (
+      <div className="container-fluid">
+        <div className="row justify-content-center">Login:</div>
+        <div className="row justify-content-center">
+          <button type="submit">Submit new user</button>
+        </div>
+      </div>
+    );
   }
 }
 
