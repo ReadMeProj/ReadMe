c3fad2222103e7f1af48ed46161d735f22e2e383
Oved Haruvi <t-ovharu@microsoft.com>
2021-05-26 08:59:42 +0300

    Added flask endpoint, Added Generic recommendation

--- internal/alg/recommendedArticlesForUser.py
+++ internal/alg/recommendedArticlesForUser.py
@@ -1,89 +1,74 @@
-import logging
 from operator import itemgetter
 
 import networkx as nx
+from flask import Flask
 from networkx.algorithms import bipartite
 
-## Constants
-NOT_ENOUGH_DATA_THRESHOLD = 5
+app = Flask(__name__)
+
+# Constants
+NOT_ENOUGH_DATA_THRESHOLD = 100
 
 
-## Function that getting the data as we get it from the api and returns a connected nx bipartite Graph
+# Function that getting the data as we get it from the api and returns a connected nx bipartite Graph
 def prep(JSON_Graph):
-    raw_graph = bipartite.random_graph(100, 1000, 0.01, directed=False)
+    raw_graph = bipartite.random_graph(1000, 10000, 0.001, directed=False)
     # TODO: take json and transform to networkx graph
 
-    connected_component = nx.algorithms.node_connected_component(raw_graph, userId)
-    connected_component_graph = raw_graph.subgraph(connected_component)
-    return connected_component_graph
+    return raw_graph
 
 
-def generic_recommendation(G , num_of_articles):
-    pass
+def generic_recommendation(raw_graph, num_of_articles):
+    articles_by_degree = []
+    for connected_component in nx.connected_components(raw_graph):
+        connected_component = nx.subgraph(raw_graph , connected_component)
+        users, articles = bipartite.sets(connected_component)
+        articles_by_degree.append(
+            sorted(filter(lambda node: node in articles, connected_component.degree), key=itemgetter(1)))
+    articles, _ = zip(*articles_by_degree)
+    return articles[:num_of_articles]
 
 
-def recommendation(JSON_Graph, userId, num_of_articles):
-    # create logger with 'spam_application'
-    logger = logging.getLogger('recommendations')
-    logger.setLevel(logging.DEBUG)
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler('recommendation.log')
-    fh.setLevel(logging.DEBUG)
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
+def get_graph_data():
+    JSON = []
+    return JSON
 
-    G = prep(JSON_Graph)
 
-    # Algorithm
-    rec_articles = set()
+@app.route("/<int:user_id>/<int:num_of_articles>", methods=['GET'])
+def recommendation(user_id, num_of_articles):
+    JSON_G = get_graph_data()
+    raw_graph = prep(JSON_G)
+    connected_component = nx.algorithms.node_connected_component(raw_graph, user_id)
+    G = raw_graph.subgraph(connected_component)
+    """ 
+    Algorithm: 
+    1. Get user's liked articles
+    2. if no data regard user - return the highest 
+    2. Compute similarity for each couple (user_liked, some article) using Jaccard 
+    3. Aggregate those scores for each article.
+    4. return the articles with the best score
+    """
     users, articles = bipartite.sets(G)
-    liked_articles_by_user = set(nx.neighbors(G, userId))
+    liked_articles_by_user = set(nx.neighbors(G, user_id))
 
     if len(liked_articles_by_user) < NOT_ENOUGH_DATA_THRESHOLD:
-        return generic_recommendation(G, num_of_articles)
+        recommended = generic_recommendation(raw_graph, num_of_articles)
+    else:
+        tuples = [(article_liked_by_user, some_article)
+                  for article_liked_by_user in liked_articles_by_user
+                  for some_article in articles
+                  if not (article_liked_by_user == some_article or some_article in liked_articles_by_user)]
 
-    tuples = [(article_liked_by_user, some_article)
-              for article_liked_by_user in liked_articles_by_user
-              for some_article in articles
-              if not (article_liked_by_user == some_article or some_article in liked_articles_by_user)]
-
-    similarity_scores = sorted(list(nx.jaccard_coefficient(G, tuples)), reverse=True, key=itemgetter(2))
-    users_likes_list, article_list, sim_score = zip(*similarity_scores)
-
-
-### Try another approach
-
-#
-# tuples = [(userId, u) for u in users][1:]
-# preds = nx.jaccard_coefficient(connected_component_graph, tuples)
-# # for u, v, p in preds:
-# #     logger.info(f"({u}, {v}) -> {p:.8f}")
-#
-#
-# user_neighbors = list(nx.neighbors(connected_component_graph, userId))
-# preds = sorted(list(preds), reverse=True, key=itemgetter(2))
-# for my_user, user, pred_score in preds:
-#     if len(rec_articles) > num_of_articles:
-#         break
-#     similiar_user_neighbors = set(connected_component_graph.neighbors(user))
-#     added_articles = similiar_user_neighbors.difference(set(user_neighbors))
-#     rec_articles = rec_articles.union(added_articles)
-#
-# rec_articles = list(rec_articles)[:num_of_articles]
-# logger.info(rec_articles)
-
-
-if __name__ == "__main__":
-    JSON_Graph = {"graph": [{"userId": 123, "articleId": 122}, {"userId": 222, "articleId": 223}]}
-    userId = 0
-    num_of_articles = 20
-
-    recommended_articles = recommendation(JSON_Graph, userId, num_of_articles)
+        similarity_scores = nx.jaccard_coefficient(G, tuples)
+        article_set_ndup = [article for article in articles if article not in liked_articles_by_user]
+        article_score_dict = dict.fromkeys(article_set_ndup, 0)
+        for sim_score in similarity_scores:
+            article_score_dict[sim_score[1]] += sim_score[2]
+        sorted_dict = sorted(article_score_dict.items(), key=itemgetter(1), reverse=True)
+        articles_sorted_by_score, scores = zip(*sorted_dict)
+        recommended = articles_sorted_by_score[:num_of_articles]
+        if len(recommended) < num_of_articles:
+            recommended.extend(generic_recommendation(raw_graph, num_of_articles - len(recommended)))
+    return {
+        "recommended_articles": recommended
+    }
