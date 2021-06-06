7b14128461bab28c1506c334293e58ed6394e12d
Oved Haruvi <t-ovharu@microsoft.com>
2021-06-07 01:14:28 +0300

    Icons for tiers and Added content to about page

--- dashboard/src/pages/AboutPage.js
+++ dashboard/src/pages/AboutPage.js
@@ -3,6 +3,9 @@ import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import circleLogo from "../assets/circle-readme.png"
+import BasicUserIcon from "../assets/BasicUser.png"
+import PremiumUserIcon from "../assets/PremiumUser.png"
+import PowerUserIcon from "../assets/PowerUser.png"
 
 
 
@@ -22,42 +25,83 @@ class AboutPage extends Component {
                     height={400}
                     src={circleLogo}
                     alt="logo"
-                    style={{alignSelf:"center"}}
+                    style={{ alignSelf: "center", paddingBottom: 50 }}
                 />
-                <p style={{ color: "black" }}>ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.ReadMe is a platform where you can get the news.
-                ReadMe is a platform where you can get the news.ReadMe is a platform where you can get the news.
-                vReadMe is a platform where you can get the news.
+                <p style={{ color: "black" }}>
+                    ReadMe is a crowd sourced web app that helps users explore the news.
+                    Our engine learns your preferences, and by using complex algorithms and the wisdom of the crowd it offers a custom made news feed for each user.
+                    You can join us by being a memeber of
                     </p>
                 <Accordion>
                     <Card>
                         <Card.Header>
                             <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                Question 1
-                            </Accordion.Toggle>
+                                How can i get ReadMe credit?
+                         </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="0">
-                            <Card.Body>Answer 1 </Card.Body>
+                            <Card.Body>
+                                <div className="cardText">
+                                    <h4>There are 3 Ways to get ReadMe Credit:</h4>
+                                    <ul>
+                                        <li>
+                                            By answering on other user's questions
+                                        </li>
+                                        <li>
+                                            By Voting on the reliability of an article
+                                        </li>
+                                        <li>
+                                            By tagging keywords of articles to help other users exploring the news more effeciently
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Card.Body>
                         </Accordion.Collapse>
                     </Card>
                     <Card>
                         <Card.Header>
                             <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                                Question 2
-      </Accordion.Toggle>
+                                What can i do with ReadMe Credit
+                         </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="1">
-                            <Card.Body>Answert 2 </Card.Body>
+                            <Card.Body>
+                                <div className="cardText">
+                                    <p>The credit system dividing ReadMe Users into tiers, your tier is calculated by your relative position among ReadMe users.</p>
+                                    <ul>
+                                        <li>
+                                            <img
+                                                width={50}
+                                                height={50}
+                                                src={BasicUserIcon}
+                                                alt="logo"
+                                                style={{ alignSelf: "left", paddingright: 30 }}
+                                            />
+                                            <span>- Basic User</span>
+                                        </li>
+                                        <li>
+                                            <img
+                                                width={50}
+                                                height={50}
+                                                src={PremiumUserIcon}
+                                                alt="logo"
+                                                style={{ alignSelf: "left", paddingright: 30 }}
+                                            />
+                                            <span>- Premium User</span>
+                                        </li>
+                                        <li>
+                                            <img
+                                                width={50}
+                                                height={50}
+                                                src={PowerUserIcon}
+                                                alt="logo"
+                                                style={{ alignSelf: "left", paddingright: 30 }}
+                                            />
+                                            <span>- Power User</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Card.Body>
                         </Accordion.Collapse>
                     </Card>
                 </Accordion>
