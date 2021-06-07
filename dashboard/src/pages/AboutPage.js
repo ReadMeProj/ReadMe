276c0b44bf56e6f4a1f97ea373502a616cb1c0bc
Oved Haruvi <t-ovharu@microsoft.com>
2021-06-07 12:55:16 +0300

    improve the about page - added content and signup

--- dashboard/src/pages/AboutPage.js
+++ dashboard/src/pages/AboutPage.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-import circleLogo from "../assets/circle-readme.png"
+import circleLogo from "../assets/SiteLogo-Circle.png"
 import BasicUserIcon from "../assets/BasicUser.png"
 import PremiumUserIcon from "../assets/PremiumUser.png"
 import PowerUserIcon from "../assets/PowerUser.png"
-
+import { isLoggedIn } from "../network/lib/apiUserFunctions";
+import { Link } from "react-router-dom";
 
 
 
@@ -25,12 +26,12 @@ class AboutPage extends Component {
                     height={400}
                     src={circleLogo}
                     alt="logo"
-                    style={{ alignSelf: "center", paddingBottom: 50 }}
+                    style={{ alignSelf: "center", paddingBottom: 50, background: "#eff0f8" }}
                 />
                 <p style={{ color: "black" }}>
                     ReadMe is a crowd sourced web app that helps users explore the news.
                     Our engine learns your preferences, and by using complex algorithms and the wisdom of the crowd it offers a custom made news feed for each user.
-                    You can join us by being a memeber of
+                    Join us! Consume the news more efficiently and help other users as well!
                     </p>
                 <Accordion>
                     <Card>
@@ -42,7 +43,7 @@ class AboutPage extends Component {
                         <Accordion.Collapse eventKey="0">
                             <Card.Body>
                                 <div className="cardText">
-                                    <h4>There are 3 Ways to get ReadMe Credit:</h4>
+                                    <h5>There are 4 Ways to get ReadMe Credit:</h5>
                                     <ul>
                                         <li>
                                             By answering on other user's questions
@@ -52,6 +53,9 @@ class AboutPage extends Component {
                                         </li>
                                         <li>
                                             By tagging keywords of articles to help other users exploring the news more effeciently
+                                        </li>
+                                        <li>
+                                            Download our chrome extension,<br /> enjoy the optimal experience and help us explore the internet to provide a better platform.
                                         </li>
                                     </ul>
                                 </div>
@@ -67,7 +71,7 @@ class AboutPage extends Component {
                         <Accordion.Collapse eventKey="1">
                             <Card.Body>
                                 <div className="cardText">
-                                    <p>The credit system dividing ReadMe Users into tiers, your tier is calculated by your relative position among ReadMe users.</p>
+                                    <h5>The credit system dividing ReadMe Users into tiers,<br /> your tier is calculated by your relative position among ReadMe users.</h5>
                                     <ul>
                                         <li>
                                             <img
@@ -77,7 +81,18 @@ class AboutPage extends Component {
                                                 alt="logo"
                                                 style={{ alignSelf: "left", paddingright: 30 }}
                                             />
-                                            <span>- Basic User</span>
+                                            <span style={{ fontWeight: "bold" }}>- Basic Users can enjoy</span>
+                                            <ul>
+                                                <li>
+                                                    Reading the news
+                                                </li>
+                                                <li>
+                                                    Explore recommended articles
+                                                </li>
+                                                <li>
+                                                    See what is fake or sponsered.
+                                                </li>
+                                            </ul>
                                         </li>
                                         <li>
                                             <img
@@ -87,7 +102,14 @@ class AboutPage extends Component {
                                                 alt="logo"
                                                 style={{ alignSelf: "left", paddingright: 30 }}
                                             />
-                                            <span>- Premium User</span>
+                                            <span style={{ fontWeight: "bold" }}>- Premium Users can enjoy</span>
+                                            <ul>
+                                                <li>
+                                                    Apply requests in article pages, our system will route you request to the optimal user for an answer.
+                                                </li>
+                                            </ul>
+                                            <ul>
+                                            </ul>
                                         </li>
                                         <li>
                                             <img
@@ -97,7 +119,11 @@ class AboutPage extends Component {
                                                 alt="logo"
                                                 style={{ alignSelf: "left", paddingright: 30 }}
                                             />
-                                            <span>- Power User</span>
+                                            <span style={{ fontWeight: "bold" }}>- Power User can enjoy</span>
+                                            <ul>
+                                                <li>Perform complex queries to readme database and news analytics.<br/>
+                                                You can see which website published most of the fake news, sort authors by favorites and many more!</li>
+                                            </ul>
                                         </li>
                                     </ul>
                                 </div>
@@ -105,6 +131,12 @@ class AboutPage extends Component {
                         </Accordion.Collapse>
                     </Card>
                 </Accordion>
+                {!isLoggedIn() && <Link
+                    className="btn btn-info"
+                    to={"signUp"}
+                    style={{ alignSelf: "center", marginTop: 50 }}
+                >
+                    Join ReadMe!</Link>}
             </div>
         )
     }
