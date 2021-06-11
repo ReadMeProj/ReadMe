c130cd380135acd24255ae8d829ef19a8819e2e8
RonitPriav <42570726+RonitPr@users.noreply.github.com>
2021-06-11 16:02:42 +0300

    added calculation of relaibility with threshold calculation for the articles shown

--- dashboard/src/pages/FeedPage.js
+++ dashboard/src/pages/FeedPage.js
@@ -11,7 +11,6 @@ import SearchFilter from "../components/SearchFilters";
 class FeedPage extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       articlesData: [],
       tagsData: [],
@@ -22,6 +21,44 @@ class FeedPage extends Component {
         }
       },
     };
+  }
+
+  calcThreshold(articles) {
+    if (articles == null || articles.length == 0) return 0;
+
+    var upAvg,
+      downAvg,
+      upSum = 0,
+      downSum = 0;
+
+    articles.forEach((element) => {
+      upSum += element.fakevotes.up;
+      downSum += element.fakevotes.down;
+    });
+    upAvg = upSum / articles.length;
+    downAvg = downSum / articles.length;
+    return this.calcRelaibility(upAvg, downAvg, null);
+  }
+
+  calcRelaibility(upVotes, downVotes, threshold) {
+    var ratio = 0;
+    if (upVotes > 0 && downVotes > 0) {
+      ratio = upVotes / downVotes;
+    } else if (upVotes > 0 && downVotes === 0) {
+      ratio = upVotes / 0.9;
+    } else if (upVotes == 0 && downVotes > 0) {
+      ratio = -1 * (downVotes / 0.9);
+    }
+    // Check threshold for both directions.
+    if (threshold) {
+      if (ratio > 0) {
+        return ratio >= threshold ? ratio : 0;
+      } else {
+        return ratio * -1 >= threshold ? ratio : 0;
+      }
+    } else {
+      return ratio;
+    }
   }
 
   async componentDidMount() {
@@ -61,9 +98,22 @@ class FeedPage extends Component {
     var articlesToMap = [];
     if (articles !== null && this.state.tagsData.length === 0) {
       if (this.state.showFirst) {
+        var threshold = this.calcThreshold(articles);
         if (this.state.showFirst === "real") {
           articlesToMap = articles
-            .sort((a, b) => b.fakevotes.up - a.fakevotes.up)
+            .sort(
+              (a, b) =>
+                this.calcRelaibility(
+                  b.fakevotes.up,
+                  b.fakevotes.down,
+                  threshold
+                ) -
+                this.calcRelaibility(
+                  a.fakevotes.up,
+                  a.fakevotes.down,
+                  threshold
+                )
+            )
             .map((article) => (
               <dd key={article.id}>
                 <ArticleCard articleId={article.id} />
@@ -71,7 +121,19 @@ class FeedPage extends Component {
             ));
         } else {
           articlesToMap = articles
-            .sort((a, b) => b.fakevotes.down - a.fakevotes.down)
+            .sort(
+              (a, b) =>
+                this.calcRelaibility(
+                  a.fakevotes.up,
+                  a.fakevotes.down,
+                  threshold
+                ) -
+                this.calcRelaibility(
+                  b.fakevotes.up,
+                  b.fakevotes.down,
+                  threshold
+                )
+            )
             .map((article) => (
               <dd key={article.id}>
                 <ArticleCard articleId={article.id} />
